f79bc853609c69a36125aba41a487420a7c2ab9f
Gustavo Franceschini de Araujo <gustavofranceschini22@gmail.com>
2023-02-04 17:16:31 -0300

    Entity class : Client

---
+++ src/models/entities/Client.java
@@ -0,0 +1,105 @@
+package models.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Client implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String name;
+	private Integer rg;
+	private Integer cpf;
+	private Date birthDate;
+	
+	private TypeClient typeClient;
+
+	public Client(Integer id, String name, Integer rg, Integer cpf, Date birthDate, TypeClient typeClient) {
+		this.id = id;
+		this.name = name;
+		this.rg = rg;
+		this.cpf = cpf;
+		this.birthDate = birthDate;
+		this.typeClient = typeClient;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getRg() {
+		return rg;
+	}
+
+	public void setRg(Integer rg) {
+		this.rg = rg;
+	}
+
+	public Integer getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(Integer cpf) {
+		this.cpf = cpf;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public TypeClient getTypeClient() {
+		return typeClient;
+	}
+
+	public void setTypeClient(TypeClient typeClient) {
+		this.typeClient = typeClient;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Client other = (Client) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Client [id=" + id + ", name=" + name + ", rg=" + rg + ", cpf=" + cpf + ", birthDate=" + birthDate
+				+ ", typeClient=" + typeClient + "]";
+	}
+}
